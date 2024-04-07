3905fd8a013e5ae51527712ae736189d8afebad3
upupnoah <upupqi.cs@gmail.com>
2024-04-07 20:47:59 +0800

    feat: add code for generating key pairs

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -1,8 +1,15 @@
 import { Keypair } from "@solana/web3.js"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers"
+import "dotenv/config"
 
-const keypair = Keypair.generate()
-console.log(`✅ Generated keypair!`)
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
-console.log(`✅ Finished!`);
+// const keypair = Keypair.generate()
+// console.log(`✅ Generated keypair!`)
+
+// console.log(`The public key is: `, keypair.publicKey.toBase58())
+// console.log(`The secret key is: `, keypair.secretKey)
+// console.log(`✅ Finished!`)
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY")
+// print keypair
+console.log(`The public key is: `, keypair.publicKey.toBase58())
